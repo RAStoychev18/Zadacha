225ef0ae8f3b9ed73a1d7cd96b7f863040cc55af
RAStoychev18 <RAStoychev18@codingburgas.bg>
2023-01-18 23:27:37 +0200

    Initial commit

---
+++ src/main/java/com/example/zadacha/MainController.java
@@ -0,0 +1,25 @@
+package com.example.zadacha;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/print-numbers")
+    public String printNumbers(Model model, @RequestParam int n) {
+
+        List<Integer> list = new ArrayList<>();
+
+        for(int i = 1; i <= n ; i++) {
+            list.add(i);
+        }
+
+        model.addAttribute("printNumb1", list);
+
+        return "printNumbers";
+    }
+}
